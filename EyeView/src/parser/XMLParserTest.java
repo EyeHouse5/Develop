cc2c3115b37a387f848802193fa8f04d4b779d00
tp669 <tp669@ELCLASS20.its.york.ac.uk>
2015-03-03 13:48:47 +0000

    Just made an unfinished test

---
+++ EyeView/src/parser/XMLParserTest.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package parser;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author tp669
+ *
+ */
+public class XMLParserTest {
+
+	
+	
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
