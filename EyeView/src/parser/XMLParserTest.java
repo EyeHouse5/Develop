0fde7376df6530b1acffe6232d53c2a437c2be5a
tp669 <tp669@ELCLASS17.its.york.ac.uk>
2015-03-03 20:33:17 +0000

    Testing complete. Test report online

--- EyeView/src/parser/XMLParserTest.java
+++ EyeView/src/parser/XMLParserTest.java
@@ -5,6 +5,8 @@ package parser;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -14,15 +16,38 @@ import org.junit.Test;
  */
 public class XMLParserTest {
 
-	
+	private XMLParser parser;
+	private List<Slide> slides;
+	private Slideshow slideshow;
 	
 	@Before
 	public void setUp() throws Exception {
+		parser = new XMLParser();
+		slideshow = parser.loadSlideshow("Example PWS XML.xml");
 	}
-
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void PaserShouldFindCollectReleventInformationAndStoreInCorrectList() {		
+		assertTrue(slideshow.getSlides().get(0) instanceof Slide);
+		assertTrue(slideshow.getSlides().get(0).getGraphicList().get(0) instanceof Graphic);
+		assertTrue(slideshow.getSlides().get(0).getImageList().get(0) instanceof Image);
+		assertTrue(slideshow.getSlides().get(0).getVideoList().get(0) instanceof Video);
+		assertTrue(slideshow.getSlides().get(0).getAudioList().get(0) instanceof Audio);
+		assertTrue(slideshow.getSlides().get(0).getTextList().get(0) instanceof Text);
 	}
-
+	
+	@Test
+	public void ListsShouldContainCorrectInformation() {
+		assertEquals(slideshow.getInfo().getAuthor(), "EyeHouse");
+		
+		assertEquals(slideshow.getTitle(), "EyeView");
+		
+		assertEquals(slideshow.getSlides().get(0).getTitle(), "First Slide");
+		assertEquals(slideshow.getSlides().get(0).getImageList().get(0).getSource(), "selfie.jpg");
+		assertEquals(slideshow.getSlides().get(0).getAudioList().get(0).getSource(), "gavel.wav");
+		
+		assertEquals(slideshow.getSlides().get(1).getTitle(), "Second Slide");
+		assertEquals(slideshow.getSlides().get(1).getImageList().get(0).getSource(), "companylogo.jpg");
+		assertEquals(slideshow.getSlides().get(1).getAudioList().get(0).getSource(), "honk.wav");
+	}
 }
